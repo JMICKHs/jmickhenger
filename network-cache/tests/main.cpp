60a4e8812e740bd18fa43dc542bc9b09d4cf6013
Sergei Alexeev <serenehet@gmail.com>
2020-04-14 19:43:00 +0300

    add gtests module

--- network-cache/tests/main.cpp
+++ network-cache/tests/main.cpp
@@ -1,4 +1,6 @@
-//
-// Created by Sergei Alexeev on 13.04.2020.
-//
+#include "gtest/gtest.h"
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ network-cache/tests/tests.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+
+TEST(testAreaComparison, test1) {
+    ASSERT_EQ(true, true);
+}
