99b45bec043132478c68619b96ada504f77fcf01
Nikita Shchelkanov <nik.shchelkanov@gmail.com>
2020-04-22 16:39:19 +0300

    change skeleton for tests

--- Tests/TestBusinessLogicProxy.cpp
+++ Tests/TestBusinessLogicProxy.cpp
@@ -1,6 +1,34 @@
 //
 // Created by nick on 15.04.2020.
 //
-
 #include "gtest/gtest.h"
 #include "../Source/BusinessLogicProxy/BusinessLogicProxy.h"
+
+#define SUCCESS 1
+#define FAIL -1
+
+
+TEST(enqueue_test, valid_data) {
+    ASSERT_EQ(enquueu(), SUCCESS);
+}
+
+TEST(enqueue_test, not_valid_data) {
+    ASSERT_EQ(enquueu(), FAIL);
+}
+
+TEST(dequeue_test, valid_data) {
+    ASSERT_EQ(dequeue(), SUCCESS);
+}
+
+TEST(dequeue_test, not_valid_data) {
+    ASSERT_EQ(dequeue(), FAIL);
+}
+
+TEST(watch_first_package_id_test, valid_data) {
+    ASSERT_EQ(watch_first_package_id(), SUCCESS);
+}
+
+TEST(watch_first_package_id_test, not_valid_data) {
+    ASSERT_EQ(watch_first_package_id(), FAIL);
+}
+
--- Tests/TestConnection.cpp
+++ Tests/TestConnection.cpp
@@ -4,3 +4,10 @@
 
 #include "gtest/gtest.h"
 #include "../Source/Connection/Connection.h"
+TEST(get_socket_test, valid_data) {
+    ASSERT_EQ(get_socket(), SUCCESS);
+}
+
+TEST(get_socket_test, not_valid_data) {
+    ASSERT_EQ(get_socket(), FAIL);
+}
