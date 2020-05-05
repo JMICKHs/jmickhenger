d607b7b9a7e8b49f37c6bb78f4777b15c1a1b888
Sergei Alexeev <serenehet@gmail.com>
2020-05-05 19:40:48 +0300

    save frame cache

--- app/netlib/cache/Cache.h
+++ app/netlib/cache/Cache.h
@@ -6,9 +6,33 @@
 #define NETLIB_CACHE_H
 
 
-class Cache {
+#include "../info/Info.h"
+#include <memory>
+#include <optional>
 
+class AbstractCache {
+public:
+    virtual bool save(inf::MyAccount & acc) = 0;
+    virtual bool save(inf::UserInfo & user) = 0;
+    virtual bool save(vector<inf::ChatInfo> & chatList) = 0;
+    virtual std::optional<inf::MyAccount> & getMyAccount() = 0;
+    virtual std::optional<inf::UserInfo> & getUser(int idUser) = 0;
+    virtual vector<inf::ChatInfo> & getChatList() = 0;
 };
+
+//class Cache: public AbstractCache {
+//public:
+//    Cache();
+//    ~Cache();
+//    bool save(inf::MyAccount & acc) override;
+//    bool save(inf::UserInfo & user) override;
+//    bool save(vector<inf::ChatInfo> & chatList) override;
+//    std::optional<inf::MyAccount> & getMyAccount() override;
+//    std::optional<inf::UserInfo> & getUser(int idUser) override;
+//    vector<inf::ChatInfo> & getChatList() override;
+//private:
+//    // sqlite3 db;
+//};
 
 
 #endif //NETLIB_CACHE_H
